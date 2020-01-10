c00701e4eda19473b4e262983e95c6cb12623c71
kmyohan0 <kmyohan0@gmail.com>
2020-01-10 15:14:50 -0800

    Modified array.sh and c-echo.cpp

--- c-echo.cpp
+++ c-echo.cpp
@@ -2,8 +2,11 @@
 
 int main(int argv, char** argc) {
 	
-	for(int i = 0; i < argv; i++) {
-		std::cout << argc[i] << " ";
+	for(int i = 1; i < argv; i++) {
+		std::cout << argc[i];
+		if (i < argv - 1) {
+			std::cout << " ";
+		}
 	}
 	std::cout << std::endl;
 }
