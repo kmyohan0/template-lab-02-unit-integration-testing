0954a3ad5624f3ff727ef2d78570cf468874775d
kmyohan0 <kmyohan0@gmail.com>
2020-01-11 00:46:10 -0800

    Added Google Test, and passed function c-echo

--- c-echo.h
+++ c-echo.h
@@ -1,12 +1,12 @@
 #include <iostream>
 
-int main(int argv, char** argc) {
-	
-	for(int i = 1; i < argv; i++) {
-		std::cout << argc[i];
-		if (i < argv - 1) {
-			std::cout << " ";
-		}
-	}
-	std::cout << std::endl;
+std::string echo(int length, char** chars) {
+    std::string ret = "";
+    for(int i = 1; i < length; i++) {
+        ret += chars[i];
+        if(i < length - 1) {
+            ret += " ";
+        }
+    }
+    return ret;
 }
